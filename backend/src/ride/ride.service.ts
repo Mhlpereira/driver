94064c6e1a7fe2f680b9bd7cd3c426305b481ea0
Mário Henrique <mariohenriquelp@gmail.com>
2024-11-22 14:41:29 -0300

    modelagem de dados no schema do prisma

---
+++ backend/src/ride/ride.service.ts
@@ -0,0 +1,11 @@
+import { CustomerEntity } from "../customer/customer.entity";
+import { DriverEntity } from "../driver/driver.entity";
+import { RideEntity } from "./ride.entity";
+
+export class rideService{
+    private rideEntity: RideEntity;
+    private driverEntity: DriverEntity[];
+    private customerEntity: CustomerEntity;
+
+    public async 
+}
---
+++ backend/todo.md
@@ -0,0 +1,8 @@
+## fazer 
+
+- configurar o prisma
+- popular o banco
+- criar os services e controllers
+- geo para mudar o endereço para coordenadas
+
+- alterar rating para objeto { avaliação com lógica de nota, descrição}
