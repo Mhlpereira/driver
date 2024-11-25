8a35dd81b6bf9db06cb7d433365c8d53ea59f53d
Mário Henrique <mariohenriquelp@gmail.com>
2024-11-25 16:44:49 -0300

    Componente navbar

--- frontend/src/component/navbar.tsx
+++ frontend/src/component/navbar.tsx
@@ -1,21 +1,31 @@
 import { Link } from "react-router-dom"
 
 import"./navbar.css" 
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 export const Navbar = () => {
+
+    const [ menu , setMenu ] = useState(false)
+
     return (
         <>
             <nav>
-                <h1 className="title">Shopper</h1>
-                <ul>
+                <h1 className="title">Driver</h1>
+                <div className="menu" onClick={() => setMenu(!menu)}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <ul className={menu ? "open" : ""}>
                     <li>
-                        <Link to="">Solicitação de viagem</Link>
+                        <NavLink to="/drive/estimate">Solicitação de viagem</NavLink>
                     </li>
                     <li>
-                        <Link to="">Opções de viagem</Link>
+                        <NavLink to="/drive/confirm">Opções de viagem</NavLink>
                     </li>
                     <li>
-                        <Link to="">Histórico de viagem</Link>
+                        <NavLink to="/drive/:customer_id">Histórico de viagem</NavLink>
                     </li>
                 </ul>
             </nav>
